bfaf8623f3330ccedd341c8b16eb33f4c0cdfbb4
Prince <chp9876@gmail.com>
2023-05-26 13:40:17 +0530

    payment integration using ccavenue

---
+++ resources/views/paymentFolder/payment-success.blade.php
@@ -0,0 +1,21 @@
+@extends('app')
+@section('title')
+    Amazepay | About
+@endsection
+@section('content')
+    <div class="container-fluid">
+        <div class="row tyfsrow">
+            <div class="col-12 text-center p-0">
+                <p>
+                    <span class="icon-tick tyicon"></span>
+                </p>
+                <h2 class="section-subtext black-header pb-3">
+                    Your Payment is successfully processed.
+                </h2>
+                <p class="pb-3">
+                    Thank you for your order
+                </p>
+            </div>
+        </div>
+    </div>
+@endsection

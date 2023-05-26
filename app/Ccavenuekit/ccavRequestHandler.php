b83a5860302c239f312b28cd85649a633984a9e5
Prince <chp9876@gmail.com>
2023-05-26 17:52:46 +0530

    secure.test.com

--- app/Ccavenuekit/ccavRequestHandler.php
+++ app/Ccavenuekit/ccavRequestHandler.php
@@ -23,7 +23,7 @@
     
     ?>
     <form method="post" name="redirect"
-        action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction">
+        action="https://secure.ccavenue.com/">
         <?php
         echo "<input type=hidden name=encRequest value=$encrypted_data>";
         echo "<input type=hidden name=access_code value=$access_code>";

aa59de5e889e64c8d748223920abab15196ba46a
shubham_toutle <shubha.toutle@gmail.com>
2023-05-27 21:50:02 +0530

    changed response way

--- routes/web.php
+++ routes/web.php
@@ -87,18 +87,18 @@ Route::post('/payment-process', function () {
 });
 
 
-// Route::get('/payment-complete', function () {
+// Route::post('/payment-complete', function () {
 //     return view('paymentFolder.ccavResponseHandler');
 // })->name('payment-complete');
-Route::match(['get', 'post'], '/payment-complete', function () {
-    return view('paymentFolder.ccavResponseHandler');
-})->name('payment-complete');
+// Route::match(['get', 'post'], '/payment-complete', function () {
+//     return view('paymentFolder.ccavResponseHandler');
+// })->name('payment-complete');
 // Route::post('/payment-process', 'App\Http\Controllers\PaymentController@processPayment')->name('payment-process');
 
 
 
 // Routes for payment success and failure actions
 
-// Route::get('/payment/success', 'App\Http\Controllers\PaymentController@paymentSuccess')->name('payment-success');
+Route::get('/payment-complete', 'App\Http\Controllers\PaymentController@paymentSuccess');
 
 // Route::get('/payment/failed', 'App\Http\Controllers\PaymentController@paymentFailed')->name('payment-failed');

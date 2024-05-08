f684d7251169c53b140aede39241ba27eb7432ce
Prince Chaurasiya <chp9876@gmail.com>
2024-05-08 11:33:58 +0530

    banner changes

--- resources/views/userpanel/index.blade.php
+++ resources/views/userpanel/index.blade.php
@@ -19,17 +19,17 @@
             <div class="carousel-inner">
                 <!-- Slide 1 -->
                 <div class="carousel-item active">
-                    <img src="{{ asset('images/merry-christmas.png') }}" alt="Amazepay Banner 1" class="d-block w-100">
+                    <img src="{{ asset('images/amazepay-desk-1.png') }}" alt="Amazepay Banner 1" class="d-block w-100">
                 </div>
 
                 <!-- Slide 2 -->
                 <div class="carousel-item">
-                    <img src="{{ asset('images/happy-new-year.png') }}" alt="Amazepay Banner 2" class="d-block w-100">
+                    <img src="{{ asset('images/amazepay-desk-2.png') }}" alt="Amazepay Banner 2" class="d-block w-100">
                 </div>
 
                 <!-- Slide 3 -->
                 <div class="carousel-item">
-                    <img src="{{ asset('images/meery-christmas-offer.png') }}" alt="Amazepay Banner 3"
+                    <img src="{{ asset('images/amazepay-desk-3.png') }}" alt="Amazepay Banner 3"
                         class="d-block w-100">
                 </div>
             </div>
@@ -58,19 +58,19 @@
             <div class="carousel-inner">
                 <!-- Slide 1 -->
                 <div class="carousel-item active">
-                    <img src="{{ asset('images/merry-christmas-mobile.png') }}" alt="Amazepay Banner 1"
+                    <img src="{{ asset('images/amazepay-mob-1.png') }}" alt="Amazepay Banner 1"
                         class="d-block w-100">
                 </div>
 
                 <!-- Slide 2 -->
                 <div class="carousel-item">
-                    <img src="{{ asset('images/happy-new-year-mobile.png') }}" alt="Amazepay Banner 2"
+                    <img src="{{ asset('images/amazepay-mob-2.png') }}" alt="Amazepay Banner 2"
                         class="d-block w-100">
                 </div>
 
                 <!-- Slide 3 -->
                 <div class="carousel-item">
-                    <img src="{{ asset('images/meery-christmas-offer-mobile.png') }}" alt="Amazepay Banner 3"
+                    <img src="{{ asset('images/amazepay-mob-3.png') }}" alt="Amazepay Banner 3"
                         class="d-block w-100">
                 </div>
             </div>

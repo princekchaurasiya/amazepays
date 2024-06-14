9918750c7f19e9f789ebf80b701c85a718eb8159
Prince Chaurasiya <chp9876@gmail.com>
2024-06-14 18:51:53 +0530

    testing type

--- resources/views/userpanel/productPage.blade.php
+++ resources/views/userpanel/productPage.blade.php
@@ -59,7 +59,7 @@
                                     <div class="row">
                                         <div class="col-lg-4">
                                             <div class="order-2 mb-3 mb-lg-0 coupon-quantity">
-                                                @dd($productDetails);
+
                                                 <!-- slab means checkbox -->
                                                 @if ($productDetails['price']->type == 'SLAB')
                                                     <label class="small-size fw-600 text-grey-900 font-xsss">Select

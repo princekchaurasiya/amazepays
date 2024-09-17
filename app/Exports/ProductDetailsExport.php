f60e384b73044f699bd5e327486aeb42fe9343ce
Prince Chaurasiya <chp9876@gmail.com>
2024-09-17 16:57:21 +0530

    hot product column removed in productdetails export asnot present in live

--- app/Exports/ProductDetailsExport.php
+++ app/Exports/ProductDetailsExport.php
@@ -9,11 +9,11 @@ class ProductDetailsExport implements FromCollection, WithHeadings, WithStyles
 {
     public function collection()
     {
-        return QsProduct::select(['product_id', 'sku', 'discounts', 'name', 'description', 'price', 'kycEnabled', 'additionalForm', 'metaInformation', 'type', 'schedulingEnabled', 'currency', 'product_currency_code', 'images', 'tnc', 'categories', 'themes', 'customThemesAvailable', 'handlingCharges', 'reloadCardNumber', 'expiry', 'formatExpiry', 'relatedProducts', 'storeLocatorUrl', 'brandName', 'etaMessage', 'cpg', 'payout', 'allowedfulfillments', 'url', 'minPrice', 'maxPrice', 'created_at', 'prdt_created_at', 'prdt_updated_at', 'qs_category_id', 'discount_percentage', 'slug', 'is_hot'])->get();
+        return QsProduct::select(['product_id', 'sku', 'discounts', 'name', 'description', 'price', 'kycEnabled', 'additionalForm', 'metaInformation', 'type', 'schedulingEnabled', 'currency', 'product_currency_code', 'images', 'tnc', 'categories', 'themes', 'customThemesAvailable', 'handlingCharges', 'reloadCardNumber', 'expiry', 'formatExpiry', 'relatedProducts', 'storeLocatorUrl', 'brandName', 'etaMessage', 'cpg', 'payout', 'allowedfulfillments', 'url', 'minPrice', 'maxPrice', 'created_at', 'prdt_created_at', 'prdt_updated_at', 'qs_category_id', 'discount_percentage', 'slug'])->get();
     }
     public function headings(): array
     {
-        return ['Product Id', 'Sku', 'Discounts', 'Name', 'Description', 'Price', 'KycEnabled', 'AdditionalForm', 'MetaInformation', 'Type', 'SchedulingEnabled', 'Currency', 'Product Currency Code', 'Images', 'Tnc', 'Categories', 'Themes', 'CustomThemesAvailable', 'HandlingCharges', 'ReloadCardNumber', 'Expiry', 'FormatExpiry', 'RelatedProducts', 'StoreLocatorUrl', 'BrandName', 'EtaMessage', 'Cpg', 'Payout', 'Allowedfulfillments', 'Url', 'MinPrice', 'MaxPrice', 'Created At', 'Prdt Created At', 'Prdt Updated At', 'Qs Category Id', 'Discount Percentage', 'Slug', 'Is Hot Product?'];
+        return ['Product Id', 'Sku', 'Discounts', 'Name', 'Description', 'Price', 'KycEnabled', 'AdditionalForm', 'MetaInformation', 'Type', 'SchedulingEnabled', 'Currency', 'Product Currency Code', 'Images', 'Tnc', 'Categories', 'Themes', 'CustomThemesAvailable', 'HandlingCharges', 'ReloadCardNumber', 'Expiry', 'FormatExpiry', 'RelatedProducts', 'StoreLocatorUrl', 'BrandName', 'EtaMessage', 'Cpg', 'Payout', 'Allowedfulfillments', 'Url', 'MinPrice', 'MaxPrice', 'Created At', 'Prdt Created At', 'Prdt Updated At', 'Qs Category Id', 'Discount Percentage', 'Slug'];
     }
     public function styles(Worksheet $sheet)
     {

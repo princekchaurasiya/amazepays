de482058bd2e5d4929108667e4bb3e47ef2ebfa9
Prince Chaurasiya <chp9876@gmail.com>
2025-01-16 21:31:14 +0530

    woohoo order unwanted code commented

--- app/Http/Controllers/WoohooOrderController.php
+++ app/Http/Controllers/WoohooOrderController.php
@@ -329,35 +329,35 @@ class WoohooOrderController extends Controller
     }
 
 
-    public function sendInvoiceTestMail()
-    {
-        // Generate the PDF without any dynamic data (static template)
-        $pdf = PDF::loadView("layouts.invoice2");
+    // public function sendInvoiceTestMail()
+    // {
+    //     // Generate the PDF without any dynamic data (static template)
+    //     $pdf = PDF::loadView("layouts.invoice2");
 
-        // Use static email and subject
-        $recipientEmail = "prince.toutle@gmail.com";
-        $recipientName = "Prince";
+    //     // Use static email and subject
+    //     $recipientEmail = "prince.toutle@gmail.com";
+    //     $recipientName = "Prince";
 
-        // Send the email
-        Mail::send(["html" => "layouts.mail2"], compact("pdf"), function ($message) use ($pdf, $recipientEmail, $recipientName) {
-            $message->from(config("companyDefaultValues.sendMailFrom"), config("companyDefaultValues.company_name"))
-                ->to($recipientEmail, $recipientName)
-                ->subject(config("companyDefaultValues.default_subject"))
-                ->attachData($pdf->output(), "invoice.pdf");
-        });
+    //     // Send the email
+    //     Mail::send(["html" => "layouts.mail2"], compact("pdf"), function ($message) use ($pdf, $recipientEmail, $recipientName) {
+    //         $message->from(config("companyDefaultValues.sendMailFrom"), config("companyDefaultValues.company_name"))
+    //             ->to($recipientEmail, $recipientName)
+    //             ->subject(config("companyDefaultValues.default_subject"))
+    //             ->attachData($pdf->output(), "invoice.pdf");
+    //     });
 
-        // Return response
-        return response()->json([
-            "message" => "Test transaction mail sent successfully!",
-            "status" => "success",
-        ]);
-    }
+    //     // Return response
+    //     return response()->json([
+    //         "message" => "Test transaction mail sent successfully!",
+    //         "status" => "success",
+    //     ]);
+    // }
 
 
-    public function viewTestMail()
-    {
-        return view ('layouts.invoice2');
-    }
+    // public function viewTestMail()
+    // {
+    //     return view ('layouts.invoice2');
+    // }
 
 
 

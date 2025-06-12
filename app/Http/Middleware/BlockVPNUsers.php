71124c6f657a54b9eb190f4731d0c80587a1fa1e
Chhaya <itsupport@amazepays.in>
2025-06-12 13:18:09 +0530

    VPN proxy block code

---
+++ app/Http/Middleware/BlockVPNUsers.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
+class BlockVPNUsers
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $ip = $request->ip();
+        //Testing VPN proxy address
+        //$ip = '104.244.72.115';
+
+        // Skip local IPs
+        if ($ip === '127.0.0.1' || $ip === '::1') {
+            return $next($request);
+        }
+
+        $client = new Client();
+
+        try {
+            $response = $client->get("http://v2.api.iphub.info/ip/{$ip}", [
+                'headers' => [
+                    'X-Key' => env('IPHub_API_KEY'),
+                ],
+            ]);
+
+            $data = json_decode($response->getBody(), true);
+
+            if (isset($data['block']) && $data['block'] > 0) {
+                // block = 1 or 2 means it's a VPN or hosting provider
+                //abort(403, 'Access denied. VPN usage is not allowed.');
+                return response()->view('errors.vpn_blocked', [], 403);
+            }
+        } catch (\Exception $e) {
+            // Log error, but don’t block access on failure
+            \Log::error('VPN Check failed: ' . $e->getMessage());
+        }
+
+        return $next($request);
+    }
+}

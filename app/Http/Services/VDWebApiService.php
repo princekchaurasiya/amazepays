c6efea501d69bbe8ec8a3de279915305129fa022
Chhaya <itsupport@amazepays.in>
2025-07-12 12:16:49 +0530

    removed throw

--- app/Http/Services/VDWebApiService.php
+++ app/Http/Services/VDWebApiService.php
@@ -295,7 +295,7 @@ public function getEvc(string $token, string $payload)
             'token' => $token,
              'Accept' => 'application/json',
             'Content-Type' => 'application/json',
-        ])->throw()->post('http://cards.vdwebapi.com/distributor/getevc', $payload);
+        ])->post('http://cards.vdwebapi.com/distributor/getevc', $payload);
 
         // Debug response
         if ($response->failed()) {

0afff04f2a2dcac9ac15ea255d742b033a2c2a02
Chhaya <itsupport@amazepays.in>
2025-08-12 17:11:15 +0530

    VD activated status

--- resources/views/evc/form.blade.php
+++ resources/views/evc/form.blade.php
@@ -18,5 +18,19 @@
 
         <button class="btn btn-primary mt-3">Check Status</button>
     </form>
+    <form method="POST" action="{{ route('evc.activated') }}">
+        @csrf
+        <div class="form-group">
+            <label>Order ID</label>
+            <input type="text" name="order_id" class="form-control" required>
+        </div>
+
+        <div class="form-group">
+            <label>Request Ref No</label>
+            <input type="text" name="request_ref_no" class="form-control" required>
+        </div>
+
+        <button class="btn btn-primary mt-3">Check Activated Status</button>
+    </form>
 </div>
 @endsection

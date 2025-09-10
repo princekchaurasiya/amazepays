2b401849c61a6d2142068e094c6972df0ba25bc3
Chhaya <itsupport@amazepays.in>
2025-09-10 16:20:10 +0530

    display order details

--- resources/views/payment/success.blade.php
+++ resources/views/payment/success.blade.php
@@ -4,7 +4,7 @@
     <div style="color: green; text-align: center; margin-top: 50px;">
         <h2>Payment Successfull</h2>
         <button 
-            onclick="window.location.href='{{ route('getevc.request') }}'" 
+            onclick="window.location.href='{{ route('evc.details') }}'" 
             style="margin-top: 20px; padding: 10px 20px; border: none; border-radius: 5px; background-color: #007bff; color: white; cursor: pointer;"
         >
             Get Order Details

859f4941a59c74dc47ef0ad7f50634dc6c9ca437
Chhaya <itsupport@amazepays.in>
2025-10-06 18:23:31 +0530

    Lessen the redirection time

--- resources/views/woohoo/redirect-to-woohoo.blade.php
+++ resources/views/woohoo/redirect-to-woohoo.blade.php
@@ -64,7 +64,7 @@
             document.getElementById('clock').textContent = secondsElapsed;
 
             // Check if elapsed time is 180 seconds
-            if (secondsElapsed === 180) {
+            if (secondsElapsed === 10) {
                 // Submit the form after 180 seconds
                 document.getElementById('redirectForm').submit();
             }
